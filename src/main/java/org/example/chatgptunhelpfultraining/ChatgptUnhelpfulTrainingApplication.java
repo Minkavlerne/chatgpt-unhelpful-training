61c50cc661ed3eeb7ac578f84a6bfd0c1c605f38
sebbex1337 <sefort@hotmail.dk>
2024-04-15 13:19:49 +0200

    init

---
+++ src/main/java/org/example/chatgptunhelpfultraining/ChatgptUnhelpfulTrainingApplication.java
@@ -0,0 +1,13 @@
+package org.example.chatgptunhelpfultraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatgptUnhelpfulTrainingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatgptUnhelpfulTrainingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/chatgptunhelpfultraining/ChatgptUnhelpfulTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.chatgptunhelpfultraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatgptUnhelpfulTrainingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
